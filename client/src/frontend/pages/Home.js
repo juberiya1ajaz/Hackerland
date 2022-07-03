701ca13a95e8dcef4d0b0ed19e6fe2ba1b8b58bf
utkarsh575 <utkarshlohani575@gmail.com>
2022-07-03 18:16:22 +0530

    :lipstick: made ui changes

--- client/src/frontend/pages/Home.js
+++ client/src/frontend/pages/Home.js
@@ -7,14 +7,15 @@ export default function Home() {
   return (
     <div className="md:mx-28 mx-4 text-white">
       <div className="md:grid md:grid-cols-2 items-center py-10">
-        <div className=" p-10 border-2 border-r-8 border-b-8 rounded-xl bg-yellow-200 text-black border-black font-bold">
+        <div className=" p-10 border-2 border-r-8 border-b-8 rounded-xl bg-yellow-200 text-black border-black font-bold  bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-60  
+">
           <h1 className="text-3xl md:text-6xl">Who we are</h1>
           <p className="text-xl md:text-2xl py-4 tracking-wider">
-            Aout the app
+            About the app
           </p>
 
           <Link to="/music">
-            <button className="bg-secondary py-2 px-8 rounded-md text-xl md:text-2xl">
+            <button className="text-white bg-secondary py-2 px-8 rounded-md text-xl md:text-2xl">
               Listen Music
             </button>
           </Link>
@@ -28,7 +29,7 @@ export default function Home() {
         <div className="grid place-items-center py-4 drop-shadow-3xl shadow-black">
           <img src={FeatImg} alt="img" width="300" height="300" />
         </div>
-        <div className=" p-10 border-2 border-r-8 border-b-8 rounded-xl bg-green-300 font-bold border-black text-black">
+        <div className="bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-60 p-10 border-2 border-r-8 border-b-8 rounded-xl bg-green-300 font-bold border-black text-black">
           <h1 className="text-3xl md:text-6xl">What else do we have</h1>
           <p className="text-xl md:text-2xl py-4 tracking-wider">
             GRooVe with NFTs is a web app where you can:
